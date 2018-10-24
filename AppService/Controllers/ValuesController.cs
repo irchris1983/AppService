d1fe541efb436e5f6ac7b8502abc93d1ec26db78
Christopher Coleman <christopher_coleman@bd.com>
2018-10-24 23:02:07 +0100

    Changed: Added API and global class

--- AppService/Controllers/ValuesController.cs
+++ AppService/Controllers/ValuesController.cs
@@ -2,26 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppService.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
-    {
+   [Route("api/[controller]")]
+   //[EnableCors("AllowSpecificOrigin")]
+   [ApiController]
+   public class ValuesController : ControllerBase
+   {
+      string _user = "No User logged in";
+
       // GET api/values
       [HttpGet]
       public ActionResult<IEnumerable<string>> Get()
       {
          return new string[] { "This is the first value", "Test of API call" };
       }
-      
-      [HttpGet("getuser/{user}")]
-      public ActionResult<IEnumerable<string>> GetUser(string user)
+
+      // PUT api/values/5
+      [HttpPut]
+      [Route("setuser/{user}")]
+      public void SetUser(string user)
       {
-         return new string[] { user };
+         Globals.User = user;
       }
+
+      //[HttpGet("getuser/{user}")]
+      [HttpGet]
+      [Route("getuser")]
+      public string GetUser()
+      {
+         return Globals.User;
+      }
+
+      //[HttpGet("getuser/{user}")]
+      //public ActionResult<IEnumerable<string>> GetUser(string user)
+      //{
+      //   return new string[] { user };
+      //}
 
 
       // GET api/values/5
@@ -33,20 +53,20 @@ namespace AppService.Controllers
 
       // POST api/values
       [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+      public void Post([FromBody] string value)
+      {
+      }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+      // PUT api/values/5
+      [HttpPut("{id}")]
+      public void Put(int id, [FromBody] string value)
+      {
+      }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
+      // DELETE api/values/5
+      [HttpDelete("{id}")]
+      public void Delete(int id)
+      {
+      }
+   }
 }
---
+++ AppService/Globals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppService
+{
+   public static class Globals
+   {
+      public static string User { get; set; }
+
+   }
+}
